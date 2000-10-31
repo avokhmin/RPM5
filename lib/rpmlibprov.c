82d6dd3ba0128c7c81036571cccf069674c8054b
jbj <jbj>
2000-10-31 18:01:46 +0000

    - add rpmGetRpmlibProvides() to retrieve rpmlib(...) provides 	"Pawel A. Gajda" <mis@k2.net.pl>.

---
+++ lib/rpmlibprov.c
@@ -0,0 +1,79 @@
+#include "system.h"
+
+#include <rpmlib.h>
+
+static struct rpmlibProvides {
+    const char * featureName;
+    const char * featureEVR;
+    int featureFlags;
+    const char * featureDescription;
+} rpmlibProvides[] = {
+    { "rpmlib(VersionedDependencies)",	"3.0.3-1",	RPMSENSE_EQUAL,
+	"PreReq:, Provides:, and Obsoletes: dependencies support versions." },
+    { "rpmlib(CompressedFileNames)",	"3.0.4-1",	RPMSENSE_EQUAL,
+	"file names stored as (dirName,BaseName,dirIndex) tuple, not as path."},
+    { "rpmlib(PayloadIsBzip2)",		"3.0.5-1",	RPMSENSE_EQUAL,
+	"package payload compressed using bzip2." },
+    { "rpmlib(PayloadFilesHavePrefix)",	"4.0-1",	RPMSENSE_EQUAL,
+	"package payload files have \"./\" prefix." },
+    { NULL,				NULL,	0 }
+};
+
+void rpmShowRpmlibProvides(FILE * fp)
+{
+    const struct rpmlibProvides * rlp;
+
+    for (rlp = rpmlibProvides; rlp->featureName != NULL; rlp++) {
+	fprintf(fp, "    %s", rlp->featureName);
+	if (rlp->featureFlags)
+	    printDepFlags(fp, rlp->featureEVR, rlp->featureFlags);
+	fprintf(fp, "\n");
+	fprintf(fp, "\t%s\n", rlp->featureDescription);
+    }
+}
+
+int rpmCheckRpmlibProvides(const char * keyName, const char * keyEVR,
+	int keyFlags)
+{
+    const struct rpmlibProvides * rlp;
+    int rc = 0;
+
+    for (rlp = rpmlibProvides; rlp->featureName != NULL; rlp++) {
+	rc = rpmRangesOverlap(keyName, keyEVR, keyFlags,
+		rlp->featureName, rlp->featureEVR, rlp->featureFlags);
+	if (rc)
+	    break;
+    }
+    return rc;
+}
+
+int rpmGetRpmlibProvides(const char ***provNames, int **provFlags,
+                         const char ***provVersions)
+{
+    char **names, **versions;
+    int *flags;
+    int n = 0;
+    
+    while (rpmlibProvides[n++].featureName != NULL)
+        ;
+
+    names = xmalloc(sizeof(*names) * n);
+    versions = xmalloc(sizeof(*versions) * n);
+    flags = xmalloc(sizeof(*flags) * n);
+    
+    n = 0;
+    while (rpmlibProvides[n].featureName != NULL) {
+        names[n] = rpmlibProvides[n].featureName;
+        flags[n] = rpmlibProvides[n].featureFlags;
+        versions[n] = rpmlibProvides[n].featureEVR;
+        n++;
+    }
+    
+    names[n] = NULL;
+    versions[n] = NULL;
+    
+    *provNames = names;
+    *provFlags = flags;
+    *provVersions = versions;
+    return n;
+}
