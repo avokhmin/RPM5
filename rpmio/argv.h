fd7988d8c4825d50904518067403295f159a8b0a
jbj <jbj>
2008-05-31 18:59:05 +0000

    - jbj: backport assorted rpmio changes from HEAD to stay in sync. - jbj: make sure the open path is set when using [bgl]zdOpen vector too. - jbj: lzdio: rework the layering to be more similar to gzdio/bzdio. - jbj: rpmrepo: fix: --lzma segfault, lzdFdopen was not saving path. - jbj: splint: annotate argvAppend differently. - jbj: add _fdopen to FDIO_t, expose fdbg(), make lzdio.c standalone. - jbj: expose XfdLink/XfdFree/XfdNew, nuke the FDIO_t vectors. - jbj: eliminate unused FDIO_t vectors. XfdLink/XfdFree/XfdNew need nuking. - jbj: create lzdio.c to carry lzma comprssion, possibly w dlopen(). - jbj: add --enable-build-gcov for CFLAGS -fprofile-arcs -ftest-coverage.

---
+++ rpmio/argv.h
@@ -0,0 +1,177 @@
+#ifndef _H_ARGV_
+#define	_H_ARGV_
+
+/** \ingroup rpmio
+ * \file rpmio/argv.h
+ */
+
+typedef	const char * ARGstr_t;
+typedef ARGstr_t * ARGV_t;
+
+typedef	int * ARGint_t;
+struct ARGI_s {
+    unsigned nvals;
+    ARGint_t vals;
+};
+typedef	struct ARGI_s * ARGI_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Print argv array elements.
+ * @param msg		output message prefix (or NULL)
+ * @param argv		argv array
+ * @param fp		output file handle (NULL uses stderr)
+ */
+void argvPrint(/*@null@*/ const char * msg, /*@null@*/ ARGV_t argv,
+		/*@null@*/ FILE * fp)
+	/*@globals fileSystem @*/
+	/*@modifies *fp, fileSystem @*/;
+
+/**
+ * Destroy an argi array.
+ * @param argi		argi array
+ * @return		NULL always
+ */
+/*@null@*/
+ARGI_t argiFree(/*@only@*/ /*@null@*/ ARGI_t argi)
+	/*@modifies argi @*/;
+
+/**
+ * Destroy an argv array.
+ * @param argv		argv array
+ * @return		NULL always
+ */
+/*@null@*/
+ARGV_t argvFree(/*@only@*/ /*@null@*/ ARGV_t argv)
+	/*@modifies argv @*/;
+
+/**
+ * Return no. of elements in argi array.
+ * @param argi		argi array
+ * @return		no. of elements
+ */
+int argiCount(/*@null@*/ const ARGI_t argi)
+	/*@*/;
+
+/**
+ * Return data from argi array.
+ * @param argi		argi array
+ * @return		argi array data address
+ */
+/*@null@*/
+ARGint_t argiData(/*@null@*/ ARGI_t argi)
+	/*@*/;
+
+/**
+ * Return no. of elements in argv array.
+ * @param argv		argv array
+ * @return		no. of elements
+ */
+int argvCount(/*@null@*/ const ARGV_t argv)
+	/*@*/;
+
+/**
+ * Return data from argv array.
+ * @param argv		argv array
+ * @return		argv array data address
+ */
+/*@null@*/
+ARGV_t argvData(/*@null@*/ ARGV_t argv)
+	/*@*/;
+
+/**
+ * Compare argv arrays (qsort/bsearch).
+ * @param a		1st instance address
+ * @param b		2nd instance address
+ * @return		result of comparison
+ */
+/*@-exportlocal@*/
+int argvCmp(const void * a, const void * b)
+	/*@*/;
+/*@=exportlocal@*/
+
+/**
+ * Sort an argv array.
+ * @param argv		argv array
+ * @param compar	strcmp-like comparison function, or NULL for argvCmp()
+ * @return		0 always
+ */
+int argvSort(ARGV_t argv, int (*compar)(const void *, const void *))
+	/*@modifies *argv @*/;
+
+/**
+ * Find an element in an argv array.
+ * @param argv		argv array
+ * @param val		string to find
+ * @param compar	strcmp-like comparison function, or NULL for argvCmp()
+ * @return		found string (NULL on failure)
+ */
+/*@dependent@*/ /*@null@*/
+ARGV_t argvSearch(ARGV_t argv, ARGstr_t val,
+		int (*compar)(const void *, const void *))
+	/*@*/;
+
+/**
+ * Add an int to an argi array.
+ * @retval *argip	argi array
+ * @param ix		argi array index (or -1 to append)
+ * @param val		int arg to add
+ * @return		0 always
+ */
+int argiAdd(/*@out@*/ ARGI_t * argip, int ix, int val)
+	/*@modifies *argip @*/;
+
+/**
+ * Add a string to an argv array.
+ * @retval *argvp	argv array
+ * @param val		string arg to append
+ * @return		0 always
+ */
+int argvAdd(/*@out@*/ ARGV_t * argvp, ARGstr_t val)
+	/*@modifies *argvp @*/;
+
+/**
+ * Append one argv array to another.
+ * @retval *argvp	argv array
+ * @param av		argv array to append (NULL does nothing)
+ * @return		0 always
+ */
+int argvAppend(/*@out@*/ ARGV_t * argvp, /*@null@*/ const ARGV_t av)
+	/*@modifies *argvp @*/;
+
+/**
+ * Split a string into an argv array.
+ * @retval *argvp	argv array
+ * @param str		string arg to split
+ * @param seps		separator characters (NULL is C isspace() chars)
+ * @return		0 always
+ */
+int argvSplit(ARGV_t * argvp, const char * str, /*@null@*/ const char * seps)
+	/*@modifies *argvp @*/;
+
+/**
+ * Concatenate an argv array into a string.
+ * @param argv		argv array
+ * @return		concatenated string
+ */
+/*@only@*/
+char * argvJoin(ARGV_t argv)
+	/*@*/;
+
+/**
+ * Read lines into an argv array.
+ * @retval *argvp	argv array
+ * @param fd		rpmio FD_t (NULL uses stdin)
+ * @return		0 on success
+ */
+int argvFgets(ARGV_t * argvp, void * fd)
+	/*@modifies *argvp, fd @*/;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _H_ARGV_ */
---
+++ rpmio/rpmurl.h
@@ -0,0 +1,204 @@
+#ifndef H_RPMURL
+#define H_RPMURL
+
+/** \ingroup rpmio
+ * \file rpmio/rpmurl.h
+ */
+
+#include <assert.h>
+
+/**
+ * Supported URL types.
+ */
+typedef enum urltype_e {
+    URL_IS_UNKNOWN	= 0,	/*!< unknown (aka a file) */
+    URL_IS_DASH		= 1,	/*!< stdin/stdout */
+    URL_IS_PATH		= 2,	/*!< file://... */
+    URL_IS_FTP		= 3,	/*!< ftp://... */
+    URL_IS_HTTP		= 4,	/*!< http://... */
+    URL_IS_HTTPS	= 5,	/*!< https://... */
+    URL_IS_HKP		= 6	/*!< hkp://... */
+} urltype;
+
+#define	URLMAGIC	0xd00b1ed0
+#define	URLSANE(u)	assert(u && u->magic == URLMAGIC)
+
+typedef /*@abstract@*/ /*@refcounted@*/ struct urlinfo_s * urlinfo;
+
+/**
+ * URL control structure.
+ */
+struct urlinfo_s {
+/*@refs@*/ int nrefs;		/*!< no. of references */
+/*@owned@*/ /*@relnull@*/
+    const char * url;		/*!< copy of original url */
+/*@owned@*/ /*@relnull@*/
+    const char * scheme;	/*!< URI scheme. */
+/*@owned@*/ /*@null@*/
+    const char * user;		/*!< URI user. */
+/*@owned@*/ /*@null@*/
+    const char * password;	/*!< URI password. */
+/*@owned@*/ /*@relnull@*/
+    const char * host;		/*!< URI host. */
+/*@owned@*/ /*@null@*/
+    const char * portstr;	/*!< URI port string. */
+/*@owned@*/ /*@null@*/
+    const char * proxyu;	/*!< FTP: proxy user */
+/*@owned@*/ /*@null@*/
+    const char * proxyh;	/*!< FTP/HTTP: proxy host */
+    int proxyp;			/*!< FTP/HTTP: proxy port */
+    int	port;			/*!< URI port. */
+    int urltype;		/*!< URI type. */
+/*@relnull@*/
+    FD_t ctrl;			/*!< control channel */
+/*@relnull@*/
+    FD_t data;			/*!< per-xfer data channel */
+
+/*@relnull@*/
+    void * capabilities;	/*!< neon: ne_server_capabilities ptr */
+/*@relnull@*/
+    void * lockstore;		/*!< neon: ne_lock_store ptr */
+/*@relnull@*/
+    void * sess;		/*!< neon: ne_session ptr */
+    off_t current;		/*!< neon: current body offset. */
+    off_t total;		/*!< neon: total body length. */
+    int connstatus;		/*!< neon: connection status. */
+#ifdef  REFERENCE
+typedef enum {
+    ne_conn_namelookup,	/* lookup up hostname (info = hostname) */
+    ne_conn_connecting,	/* connecting to host (info = hostname) */
+    ne_conn_connected,	/* connected to host (info = hostname) */
+    ne_conn_secure	/* connection now secure (info = crypto level) */
+} ne_conn_status;
+#endif
+
+    int bufAlloced;		/*!< sizeof I/O buffer */
+/*@owned@*/
+    char * buf;			/*!< I/O buffer */
+    int openError;		/*!< Type of open failure */
+    int httpVersion;
+    int allow;
+#define	RPMURL_SERVER_HASRANGE		( 1 << 0)
+#define	RPMURL_SERVER_HASDAVCLASS1	( 1 << 1)
+#define	RPMURL_SERVER_HASDAVCLASS2	( 1 << 2)
+#define	RPMURL_SERVER_HASDAVEXEC	( 1 << 3)
+
+#define	RPMURL_SERVER_HASDAV	(RPMURL_SERVER_HASDAVCLASS1|RPMURL_SERVER_HASDAVCLASS2|RPMURL_SERVER_HASDAVEXEC)
+    int magic;
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*@unchecked@*/
+extern int _url_count;		/*!< No. of cached URL's. */
+
+/*@unchecked@*/
+/*@only@*/ /*@null@*/
+extern urlinfo * _url_cache;	/*!< URL cache. */
+
+/*@unchecked@*/
+extern int _url_iobuf_size;	/*!< Initial size of URL I/O buffer. */
+#define RPMURL_IOBUF_SIZE	4096
+
+/*@unchecked@*/
+extern int _url_debug;		/*!< URL debugging? */
+#define RPMURL_DEBUG_IO		0x40000000
+#define RPMURL_DEBUG_REFS	0x20000000
+
+
+/**
+ * Create a URL control structure instance.
+ * @param msg		debugging identifier (unused)
+ * @return		new instance
+ */
+/*@unused@*/ urlinfo	urlNew(const char * msg)	/*@*/;
+
+/** @todo Remove debugging entry from the ABI. */
+urlinfo	XurlNew(const char * msg, const char * file, unsigned line)	/*@*/;
+#define	urlNew(_msg) XurlNew(_msg, __FILE__, __LINE__)
+
+/**
+ * Reference a URL control structure instance.
+ * @param u		URL control structure
+ * @param msg		debugging identifier (unused)
+ * @return		referenced instance
+ */
+/*@unused@*/ urlinfo	urlLink(urlinfo u, const char * msg)
+	/*@modifies u @*/;
+
+/** @todo Remove debugging entry from the ABI. */
+urlinfo	XurlLink(urlinfo u, const char * msg, const char * file, unsigned line)
+	/*@modifies u @*/;
+#define	urlLink(_u, _msg) XurlLink(_u, _msg, __FILE__, __LINE__)
+
+/**
+ * Dereference a URL control structure instance.
+ * @param u		URL control structure
+ * @param msg		debugging identifier (unused)
+ * @return		dereferenced instance (NULL if freed)
+ */
+/*@unused@*/ urlinfo	urlFree( /*@killref@*/ urlinfo u, const char * msg)
+	/*@globals fileSystem, internalState @*/
+	/*@modifies u, fileSystem, internalState @*/;
+
+/** @todo Remove debugging entry from the ABI. */
+urlinfo	XurlFree( /*@killref@*/ urlinfo u, const char * msg,
+		const char * file, unsigned line)
+	/*@globals fileSystem, internalState @*/
+	/*@modifies u, fileSystem, internalState @*/;
+#define	urlFree(_u, _msg) XurlFree(_u, _msg, __FILE__, __LINE__)
+
+/**
+ * Free cached URL control structures.
+ */
+void urlFreeCache(void)
+	/*@globals _url_cache, _url_count, fileSystem, internalState @*/
+	/*@modifies _url_cache, _url_count, fileSystem, internalState @*/;
+
+/**
+ * Return type of URL.
+ * @param url		url string
+ * @return		type of url
+ */
+urltype	urlIsURL(const char * url)
+	/*@*/;
+
+/**
+ * Return path component of URL.
+ * @param url		url string
+ * @retval pathp	pointer to path component of url
+ * @return		type of url
+ */
+/*@-incondefs@*/
+urltype	urlPath(const char * url, /*@out@*/ const char ** pathp)
+	/*@ensures maxSet(*pathp) == 0 /\ maxRead(*pathp) == 0 @*/
+	/*@modifies *pathp @*/;
+/*@=incondefs@*/
+
+/**
+ * Parse URL string into a control structure.
+ * @param url		url string
+ * @retval uret		address of new control instance pointer
+ * @return		0 on success, -1 on error
+ */
+int urlSplit(const char * url, /*@out@*/ urlinfo * uret)
+	/*@globals h_errno, internalState @*/
+	/*@modifies *uret, internalState @*/;
+
+/**
+ * Copy data from URL to local file.
+ * @param url		url string of source
+ * @param dest		file name of destination
+ * @return		0 on success, otherwise FTPERR_* code
+ */
+int urlGetFile(const char * url, /*@null@*/ const char * dest)
+	/*@globals h_errno, fileSystem, internalState @*/
+	/*@modifies fileSystem, internalState @*/;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* H_RPMURL */
